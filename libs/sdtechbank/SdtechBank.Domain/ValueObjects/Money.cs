c866f70ab78cbc888aac8bdd8491c2f4dceff4bf
Samuel Diogo de Jesus <samuel@sdtech.com.br>
2026-03-24 17:31:08 -0300

    feat: created VO Money #11

---
+++ libs/sdtechbank/SdtechBank.Domain/ValueObjects/Money.cs
@@ -0,0 +1,37 @@
+﻿using SdtechBank.Domain.Enums;
+
+namespace SdtechBank.Domain.ValueObjects;
+public sealed record Money
+{
+    public decimal Amount { get; init; }
+    public CurrencyEnum Currency { get; init; }
+
+    public Money(decimal amount, CurrencyEnum currency)
+    {
+        if (amount <= 0)
+            throw new ArgumentException("Valor deve ser maior que zero");
+        Amount = amount;
+        Currency = currency;
+    }
+
+    public Money Add(Money other)
+    {
+        if(Currency != other.Currency)
+            throw new InvalidOperationException("Moedas diferentes");
+
+        return new Money(Amount + other.Amount, Currency);
+    }
+
+    public Money Subtract(Money other)
+    {
+        if (Currency != other.Currency)
+            throw new InvalidOperationException("Moedas diferentes");
+
+        var result = Amount - other.Amount;
+
+        if (result < 0)
+            throw new InvalidOperationException("Saldo insuficiente");
+        return new Money(result, Currency);
+
+    }
+}
