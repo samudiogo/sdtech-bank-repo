0992671e16d161dc3dabea06ec80b127b36cfa3c
Samuel Diogo de Jesus <samuel@sdtech.com.br>
2026-04-19 10:51:31 +0000

    feat: added integration tests for RabbitMqPublisher

---
+++ libs/sdtechbank/SdtechBank.Infrastructure.IntegrationTests/Shared/InfraCollection.cs
@@ -0,0 +1,6 @@
+﻿
+using SdtechBank.Infrastructure.IntegrationTests.Messaging;
+namespace SdtechBank.Infrastructure.IntegrationTests.Shared;
+
+[CollectionDefinition("Infra")]
+public class InfraCollection : ICollectionFixture<RedisFixture>, ICollectionFixture<RabbitMqFixture>, ICollectionFixture<MongoDbFixture> { }
---
+++ libs/sdtechbank/SdtechBank.Infrastructure.IntegrationTests/Shared/MongoDbFixture.cs
@@ -0,0 +1,18 @@
+﻿using Testcontainers.MongoDb;
+
+namespace SdtechBank.Infrastructure.IntegrationTests.Shared;
+
+public sealed class MongoDbFixture : IAsyncLifetime
+{
+    private readonly MongoDbContainer _container = new MongoDbBuilder("mongo:8").Build();
+    public string ConnectionString => _container.GetConnectionString();
+
+    public async ValueTask InitializeAsync()
+    {
+        await _container.StartAsync();
+    }
+    public async ValueTask DisposeAsync()
+    {
+        await _container.DisposeAsync();
+    }
+}
